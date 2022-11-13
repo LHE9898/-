7d6afab13ac1126738fe683321c09d89cc68a2de
LHE9898 <92521680+LHE9898@users.noreply.github.com>
2022-11-13 23:28:01 +0900

    Update HW1.c

--- HW1.c
+++ HW1.c
@@ -66,21 +66,36 @@ int evaluate(TreeNode *root) {
 
 int main(){
     
-    TreeNode n1={3, NULL, NULL};
-    TreeNode n2={8, NULL, NULL};
-    TreeNode n3={2, NULL, NULL};
-    TreeNode n4={'/', &n2, &n3};
-    TreeNode n5={2, NULL, NULL};
-    TreeNode n6={'/', &n4, &n5};
-    TreeNode n7={'*', &n1, &n6};
-    TreeNode n8={4, NULL, NULL};
-    TreeNode n9={'+', &n7, &n8};
-    TreeNode *root= &n9;
+    TreeNode *n1, *n2, *n3, *n4, *n5, *n6, *n7, *n8, *n9, *root;
+    n1= (TreeNode *)malloc(sizeof(TreeNode));
+    n2= (TreeNode *)malloc(sizeof(TreeNode));
+    n3= (TreeNode *)malloc(sizeof(TreeNode));
+    n4= (TreeNode *)malloc(sizeof(TreeNode));
+    n5= (TreeNode *)malloc(sizeof(TreeNode));
+    n6= (TreeNode *)malloc(sizeof(TreeNode));
+    n7= (TreeNode *)malloc(sizeof(TreeNode));
+    n8= (TreeNode *)malloc(sizeof(TreeNode));
+    n9= (TreeNode *)malloc(sizeof(TreeNode));
+    root = n9;
     
-    preorder(root); printf("\n");
-    inorder(root); printf("\n");
-    postorder(root); printf("\n"); 
+    n1->data = 3;   n1->left = NULL; n1->right = NULL;
+    n2->data = 8;   n2->left = NULL; n2->right = NULL;
+    n3->data = 2;   n3->left = NULL; n3->right = NULL;
+    n4->data = '/'; n4->left = n2;   n4->right = n3;
+    n5->data = 2;   n5->left = NULL; n5->right = NULL;
+    n6->data = '/'; n6->left = n4;   n6->right = n5;
+    n7->data = '*'; n7->left = n1;   n7->right = n6;
+    n8->data = 4;   n8->left = NULL; n8->right = NULL;
+    n9->data = '+'; n9->left = n7;   n9->right = n8;
+    
+    printf("전위표기식: ");  preorder(root);  printf("\n");
+    printf("중위표기식: ");   inorder(root);   printf("\n");
+    printf("후위표기식: "); postorder(root); printf("\n"); 
     printf("수식은 %d입니다. \n", evaluate(root));
+
+    free(n1); free(n2); free(n3); 
+    free(n4); free(n5); free(n6);
+    free(n7); free(n8); free(n9);
    
     return 0;
 }
